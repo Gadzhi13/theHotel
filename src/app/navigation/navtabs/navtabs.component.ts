093555fb5fcbeedf1e2c532df6561cf5058f174a
Gadzhi13 <36446389+Gadzhi13@users.noreply.github.com>
2020-01-27 17:36:05 +0100

    new routing, new tabs

--- src/app/navigation/navtabs/navtabs.component.ts
+++ src/app/navigation/navtabs/navtabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navtabs',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navtabs.component.css']
 })
 export class NavtabsComponent implements OnInit {
+  navLinks;
+  currentUrl;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    this.navLinks = [];
+    this.router.events.subscribe(
+      (change) => {
+        this.currentUrl = this.router.url;
+      }
+    );
   }
 
+
+  ngOnInit() {
+    this.navLinks.push(
+      {path: "/", label: "Home"},
+      {path: "/accomodation", label: "Accomodation"},
+      {path: "/food-and-beverage", label: "Food and Beverage"},
+      {path: "/lifestyle", label: "Lifestyle"},
+      {path: "/celebrations", label: "Celebrations"},
+      {path: "/offers", label: "Offers"}
+    );
+  }
 }
